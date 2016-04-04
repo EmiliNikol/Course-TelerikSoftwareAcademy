05b5a46ec8fe1bf58221ba2be7c2fea4aab45cb2
Ivan <ivanviagov@abv.bg>
2016-04-04 23:06:44 +0300

    Exercises for Exam preparation.

--- BGCoder Exams/AstrologicalDigits/AstrologicalDigits.cs
+++ BGCoder Exams/AstrologicalDigits/AstrologicalDigits.cs
@@ -10,62 +10,38 @@ class AstrologicalDigits
     static void Main()
     {
         string n = Console.ReadLine();
-        n = n.TrimStart('-');
 
-        SolutionWithString(n);
-        return;
-
-        string[] number = n.Split('.');
         int sumDigits = 0;
-        BigInteger digits = 0;
-
-        for (int i = 0; i < number.Length; i++)
+        int currentDigit = 0;
+        
+        for (int i = 0; i < n.Length; i++)
         {
-            digits = BigInteger.Parse(number[i]);
-
-            while (digits > 0)
-            {
-                sumDigits += (int)(digits % 10);
-                digits /= 10;
-            }
-        }
-        do 
-        {
-            digits = sumDigits;
-            sumDigits = 0;
-
-            while (digits > 0)
-            {
-                sumDigits += (int)digits % 10;
-                digits /= 10;
-            }
-        }
-        while (sumDigits > 9);
-        Console.WriteLine(sumDigits);
-    }
-
-    private static void SolutionWithString(string n)
-    {
-        int result = 0;
-
-        for (int letterIndex = 0; letterIndex < n.Length; letterIndex++)
-        {
-            if (!Char.IsDigit(n[letterIndex]))
+            if (!Char.IsDigit(n[i]))
             {
                 continue;
             }
 
-            int currentDigit = n[letterIndex] - '0';
-            result += currentDigit;
+            currentDigit = n[i] - '0';
+            sumDigits += currentDigit;
         }
 
-        if (result > 9)
+        if (sumDigits > 9)
         {
-            SolutionWithString(result.ToString());
-            return;
+            do
+            {
+                n = Convert.ToString(sumDigits);
+                sumDigits = 0;
+                currentDigit = 0;
+
+                for (int i = 0; i < n.Length; i++)
+                {
+                    currentDigit = n[i] - '0';
+                    sumDigits += currentDigit;
+                }
+            }
+            while (sumDigits > 9);
         }
 
-        Console.WriteLine(result);
+        Console.WriteLine(sumDigits);
     }
-
 }
